c1d051d6d5a581aa61514f4cccc97faa20aad62c
KPManjula <t3kama07@students.oamk.fi>
2025-04-20 18:25:22 +0300

    videoScreen button update

--- app/src/main/java/com/example/kielibuddy/ui/pages/VideoCallScreen.kt
+++ app/src/main/java/com/example/kielibuddy/ui/pages/VideoCallScreen.kt
@@ -274,7 +274,7 @@ fun VideoCallScreen(navController: NavController, channelName: String, appId: St
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 32.dp),
+                .padding(bottom = 16.dp),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
